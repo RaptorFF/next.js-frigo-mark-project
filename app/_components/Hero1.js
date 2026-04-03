ff4df565a0a4175cbac4348af681c5de4ed0f6c0
RaptorFF <f.filipovic84@gmail.com>
2026-04-03 17:21:08 +0200

    Update Hero1

--- app/_components/Hero1.js
+++ app/_components/Hero1.js
@@ -8,7 +8,7 @@ export default function Hero1() {
         playsInline
         className="absolute top-1/2 left-1/2 min-w-full min-h-full -translate-x-1/2 -translate-y-1/2 object-cover z-[-1]"
       >
-        <source src="airflow.mp4" type="video/mp4" />
+        <source src="/airflow.mp4" type="video/mp4" />
       </video>
     </section>
   );
