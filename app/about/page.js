af07d26d1e2230a0ebbab9210744c4a06d56f3d8
RaptorFF <f.filipovic84@gmail.com>
2026-04-20 15:51:10 +0200

    Updating About page

--- app/about/page.js
+++ app/about/page.js
@@ -4,11 +4,18 @@ export default function AboutPage() {
   return (
     <section className="max-w-7xl mx-auto pt-32 pb-20 px-6 lg:px-8">
       <div className="space-y-12">
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-12 items-center">
-          <div>
-            <h1 className="text-4xl md:text-5xl font-bold text-blue-900 mb-12 tracking-tight text-left">
-              O nama
-            </h1>
+        <div>
+          <h1 className="text-4xl md:text-5xl font-bold text-blue-900 mb-8 tracking-tight text-left">
+            O nama
+          </h1>
+          <div className="overflow-hidden">
+            <Image
+              src="/ac-installation.jpeg"
+              alt="Ugradnja klima uredjaja"
+              width={1200}
+              height={720}
+              className="float-right ml-8 mb-4 w-full md:w-1/2 h-72 md:h-88 object-cover rounded-2xl shadow-lg"
+            />
             <p className="text-lg text-gray-700 mb-6 leading-relaxed">
               Naša firma specijalizovana je za profesionalnu ugradnju,
               servisiranje i održavanje klima uređaja, kao i drugih rashladnih
@@ -22,47 +29,32 @@ export default function AboutPage() {
               individualno, nudeći optimalna rešenja prema njegovim potrebama.
             </p>
           </div>
-
-          <div>
-            <Image
-              src="/ac-installation.jpeg"
-              alt="Ugradnja klima uredjaja"
-              width={1200}
-              height={720}
-              className="w-full h-75 md:h-96 object-cover rounded-2xl shadow-lg"
-            />
-          </div>
         </div>
 
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-12 items-center">
-          <div>
-            <Image
-              src="/ac-service.jpg"
-              alt="Servis klima uredjaja"
-              width={1200}
-              height={720}
-              className="w-full h-75 md:h-96 object-cover rounded-2xl shadow-lg"
-            />
-          </div>
-
-          <div>
-            <p className="text-lg text-gray-700 mb-6 leading-relaxed">
-              Pored klimatizacije i rashladnih uređaja, specijalizovani smo i za
-              servisiranje sistema toplotnih pumpi, veš mašina i električnih
-              šporeta. Naš tim poseduje znanje i iskustvo za brzo i efikasno
-              rešavanje svih vrsta kvarova i održavanje ovih uređaja, uz
-              korišćenje kvalitetnih delova i savremenih dijagnostičkih alata.
-            </p>
-            <p className="text-lg text-gray-700 mb-6 leading-relaxed">
-              Poverite nam brigu o vašoj klimatizaciji, rashladnim uređajima,
-              toplotnim pumpama, veš mašinama i električnim šporetima -
-              sigurnost, pouzdanost i zadovoljstvo klijenata su naš prioritet.
-            </p>
-            <p className="text-lg text-gray-700 leading-relaxed">
-              Za sva pitanja, savete ili zakazivanje servisa, slobodno nas
-              kontaktirajte. Hvala što ste izabrali našu firmu!
-            </p>
-          </div>
+        <div className="overflow-hidden">
+          <Image
+            src="/ac-service.jpg"
+            alt="Servis klima uredjaja"
+            width={1200}
+            height={720}
+            className="float-left mr-8 mb-4 w-full md:w-1/2 h-72 md:h-88 object-cover rounded-2xl shadow-lg"
+          />
+          <p className="text-lg text-gray-700 mb-6 leading-relaxed">
+            Pored klimatizacije i rashladnih uređaja, specijalizovani smo i za
+            servisiranje sistema toplotnih pumpi, veš mašina i električnih
+            šporeta. Naš tim poseduje znanje i iskustvo za brzo i efikasno
+            rešavanje svih vrsta kvarova i održavanje ovih uređaja, uz
+            korišćenje kvalitetnih delova i savremenih dijagnostičkih alata.
+          </p>
+          <p className="text-lg text-gray-700 mb-6 leading-relaxed">
+            Poverite nam brigu o vašoj klimatizaciji, rashladnim uređajima,
+            toplotnim pumpama, veš mašinama i električnim šporetima - sigurnost,
+            pouzdanost i zadovoljstvo klijenata su naš prioritet.
+          </p>
+          <p className="text-lg text-gray-700 leading-relaxed">
+            Za sva pitanja, savete ili zakazivanje servisa, slobodno nas
+            kontaktirajte. Hvala što ste izabrali našu firmu!
+          </p>
         </div>
       </div>
     </section>
