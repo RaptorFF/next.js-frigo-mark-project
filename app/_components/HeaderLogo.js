e002053d35bc2e196ef18013f6cd55b6d1cefd77
RaptorFF <f.filipovic84@gmail.com>
2026-05-25 13:08:56 +0200

    Adding Frigomark Logo

---
+++ app/_components/HeaderLogo.js
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import logo from "@/public/logo.png"; // koristi SVG verziju
+
+export default function HeaderLogo() {
+  return (
+    <div className="header-logo">
+      <Image
+        src={logo}
+        alt="Frigomark logo"
+        width={180} // osnovna širina
+        height={60} // visina logotipa
+        style={{
+          maxWidth: "100%",
+          height: "auto",
+          transform: "scale(1.3)", // povećava prikaz bez menjanja headera
+          transformOrigin: "center",
+        }}
+        priority
+      />
+    </div>
+  );
+}
